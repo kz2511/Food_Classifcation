90212cf93df4cdbea88b3726f0b7151abb082156
kunal zaveri <kz251199@gmail.com>
2021-11-11 17:33:00 +0530

    Working video suggestions

---
+++ video.py
@@ -0,0 +1,11 @@
+Mysore_Pak = [['Mysore Pak Recipe For Beginners','https://www.youtube.com/watch?v=XZOo2L2gKzo'],
+              ['ghee mysore pak recipe','https://www.youtube.com/watch?v=2E4GG6p48L0'],
+              ['Mysore Pak Video Recipe ','https://www.youtube.com/watch?v=J_u5-SBxaz4'],
+              ['Mysore Pak Recipe - How To Make Mysore Pak','https://www.youtube.com/watch?v=s9p3jgC4Toc'],
+              ['एक सीक्रेट टिप से जालीदार मैसूर पाक बनाये','https://www.youtube.com/watch?v=pLaHZoBPmVA']]
+
+Ghevar = [['Malai Ghevar','https://www.youtube.com/watch?v=zIKIw4lkSoM'],
+          ['Rajasthani Malai Ghevar','https://www.youtube.com/watch?v=2B1w_Ndhcjk'],
+          ['how to make ghevar - easy ghevar recipe - homemade ghevar','https://www.youtube.com/watch?v=no0XCVRdv5Y'],
+          ['how to make ghewar at home','https://www.youtube.com/watch?v=8bw1uVGAatw'],
+          ['Ghevar Recipe Perfect Halwai Style Ghewar Recipe','https://www.youtube.com/watch?v=tgdrU3YIddI']]
