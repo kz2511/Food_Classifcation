13ecff9831d8111e734e139877ee009da86341e1
kunal zaveri <kz251199@gmail.com>
2021-11-11 21:10:12 +0530

    Working video suggestions

--- video.py
+++ video.py
@@ -9,3 +9,51 @@ Ghevar = [['Malai Ghevar','https://www.youtube.com/watch?v=zIKIw4lkSoM'],
           ['how to make ghevar - easy ghevar recipe - homemade ghevar','https://www.youtube.com/watch?v=no0XCVRdv5Y'],
           ['how to make ghewar at home','https://www.youtube.com/watch?v=8bw1uVGAatw'],
           ['Ghevar Recipe Perfect Halwai Style Ghewar Recipe','https://www.youtube.com/watch?v=tgdrU3YIddI']]
+
+SohanPapdi = [['how to make soan papri at home','https://www.youtube.com/watch?v=AOmVczqYIrg'],
+              ['HALDIRAM WALI SOAN PAPDI RECIPE सबसे आसान','https://www.youtube.com/watch?v=jTTrj40VJnI'],
+              ['Soan Papdi Recipe','https://www.youtube.com/watch?v=ky0nh_-Fl2g'],
+              ['soan papadi recipe Patisa','https://www.youtube.com/watch?v=-fyfa1eOKFs'],
+              ['Flaky Soan Papdi','https://www.youtube.com/watch?v=2gkhKAvBwnw']]
+
+RasMalai = [['Rasmalai Recipe | Step by Step Rasmalai Recipe','https://www.youtube.com/watch?v=znefLMNyVE8'],
+            ['Rasmalai Roshmalai Halwai','https://www.youtube.com/watch?v=LECpZPkP9Wk'],
+            [' Soft, Juicy Rasmalai at Home ','https://www.youtube.com/watch?v=9mm8my_NLlY'],
+            ['Rasmalai Recipe','https://www.youtube.com/watch?v=DSocv7qa_3U'],
+            ['Rasmalai Halwai Style with Secrets','https://www.youtube.com/watch?v=qmooLr-IVe0']]
+
+Ariselu = [['Ariselu Recipe','https://www.youtube.com/watch?v=i4QGIMdSzig'],
+           ['Special Arisalu with Ghee Recipe','https://www.youtube.com/watch?v=PZRvGkAkHR4'],
+           ['ARISALLU - Mrs Vahchef','https://www.youtube.com/watch?v=ApDgnNeYLmY'],
+           ['dhirasam !! Tasty !!Traditional','https://www.youtube.com/watch?v=LcA-9Wk9z_A'],
+           ['Perfect Adhirasam ','https://www.youtube.com/watch?v=mYxIotMspSM']]
+
+Kofta = [['Lauki Kofta recipe in Hindi Dudhi Kofta recipe','https://www.youtube.com/watch?v=oBR3nQUnhJc'],
+         ['Lauki Kofta Recipe','https://www.youtube.com/watch?v=487VfUaTUdc'],
+         [' Bottle Gourd Kofta Curry','https://www.youtube.com/watch?v=soUKwdZS2zA'],
+         ['रेस्टौरंट जैसा मलाई कोफ़्ता बनाने का आसान तरीका -Malai Kofta Recipe','https://www.youtube.com/watch?v=Q4cSiiYa6_w'],
+         ['malai kofta recipe restaurant style','https://www.youtube.com/watch?v=c8Lh4vzSQG0']]
+
+AlooTikki = [['बाज़ार जैसी कुरकुरी आलू टिक्की बनाने की विधि','https://www.youtube.com/watch?v=JOcnHa8FnBM'],
+             ['aloo tikki recipe','https://www.youtube.com/watch?v=BrYPnqA7eIg'],
+             ['Aloo Tikki recipe','https://www.youtube.com/watch?v=Er9CaLjTouA'],
+             ['Aloo Tikki Recipe_बाज़ार जैसी कुरकुरी आलू टिक्की','https://www.youtube.com/watch?v=dYvJBSb2X7o'],
+             ['घर पर बनाए बाज़ार जैसी आलू टिक्की चाट | Aloo Tikki Chaat','https://www.youtube.com/watch?v=zstT0kgIpP8']]
+
+GajarKaHalwa = [['Gajar Ka Halwa Recipe-Simple and Delicious Gajar Halwa-Carrot Halwa Recipe-Easy Indian Dessert','https://www.youtube.com/watch?v=0DDJ4mpSSfs'],
+                ['Gajar Ka Halwa banane ka tarika_गाजर का हलवा','https://www.youtube.com/watch?v=CNJBC-TJJTA'],
+                ['Gajar Ka Halwa','https://www.youtube.com/watch?v=DLT7hC3oQFg'],
+                ['Gajar Ka Halwa Recipe','https://www.youtube.com/watch?v=LKzDV94y4p4'],
+                ['Halwai Style Gajar Ka Halwa Recipe','https://www.youtube.com/watch?v=-B1WBZqJQww']]
+
+chhenaKheeri = [['Chena Kheer Recipe - Angoor Malai','https://www.youtube.com/watch?v=jWPjm6gW_WU'],
+                ['Chhena Kheer Recipe','https://www.youtube.com/watch?v=5iO5eQlF2nY'],
+                ['Chena Kheer Recipe','https://www.youtube.com/watch?v=xIhZXdTWOEk'],
+                ['Chena Payas Recipe','https://www.youtube.com/watch?v=ba9zFDH3DMk'],
+                ['Chhena Kheer Recipe','https://www.youtube.com/watch?v=v7ZpZbbTJeY']]
+
+KakinadaKhaja = [['How to Make Kakinada Khaja','https://www.youtube.com/watch?v=kCnXvlhipko'],
+                 ['Kakinada Kaja Recipe','https://www.youtube.com/watch?v=ZOq4QPkPKNI'],
+                 ['Khaja Recipe','https://www.youtube.com/watch?v=gJMWgrECz30'],
+                 ['Khaja Recipe','https://www.youtube.com/watch?v=hRVt8OYDNDY'],
+                 ['Kakinada Kaja Recipe','https://www.youtube.com/watch?v=ZOq4QPkPKNI']]
